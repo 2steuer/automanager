9bd000fd47707be13c581f24767e35077c8757a0
Merlin Steuer <2steuer@informatik.uni-hamburg.de>
2020-12-28 17:36:53 +0100

    - Moving to PathBase

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace automanager
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("http://localhost:5000", "http://0.0.0.0:5000");
+                    webBuilder.UseUrls("http://0.0.0.0:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
--- Startup.cs
+++ Startup.cs
@@ -36,6 +36,7 @@ namespace automanager
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase(Configuration["PathBase"]);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
